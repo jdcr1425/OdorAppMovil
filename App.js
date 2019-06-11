5f8a80cbd7db0fe86ceaf3a06798ad746bb2cee7
jdcr1425 <juandavidcr7@gmail.com>
2019-06-10 23:10:04 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View,Image,StatusBar, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View,Image,StatusBar, Button  } from 'react-native';
 import Header from './src/components/Header.js';
+import Constants from 'expo-constants'
 export default class App extends React.Component{
 
   constructor() {
     super();
     this.state ={}
+    
   }
   render(){
   return (
-    <View>
+    <View style={{flex:1}}>
+      <View style={styles.statusBar} />
       <Header/>
       <View style={styles.container}>
-        <Text style={styles.question}>¿Percibe algún olor?</Text>
+        <Text style={styles.textWithShadow}>¿Percibe algún olor?</Text>
+        <View style={styles.containerButton}>
+          <Button
+            title="Sí"/>
+          <Button
+            title="No"/>
+        </View>
       </View>
     </View>
      
@@ -22,6 +31,28 @@ export default class App extends React.Component{
 }
 
 const styles = StyleSheet.create({
-      container:{flex: 1, justifyContent:'center', alignItems:'center'},
-      question:{ fontSize: 40, fontWeight: 'bold'}
+      container:{
+        flex:1,
+        justifyContent:'center', 
+        alignItems:'center',
+        backgroundColor:'#7a7cff'
+      },
+      statusBar: {
+        backgroundColor: "#0026ca",
+        height: Constants.statusBarHeight,
+      },
+      textWithShadow:{
+        color:'white',
+        fontSize: 40, 
+        fontWeight: 'bold',
+        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10
+    },
+    containerButton:{
+
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      backgroundColor:'red'
+    }
 });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import {View, Image, StyleSheet, Text, SafeAreaView} from 'react-native'
+import Constants from 'expo-constants'
+
+
+  
+  
 
 export default class Header extends Component{
     render(){
 
         return(
-                <SafeAreaView>
+                <View style={styles.header}>
                     <Image source={require('../../assets/logo.png')} style={styles.logo}/>
-                </SafeAreaView>
+                </View>
         )
     }
 }
@@ -17,6 +22,12 @@ const styles = StyleSheet.create({
         width:80,
         height: 26,
         resizeMode:'contain'
-
-    }
+    },
+    header:{
+        paddingHorizontal:15,
+        paddingVertical:15,
+        backgroundColor:'#304ffe'
+        
+    }, 
+   
 });
