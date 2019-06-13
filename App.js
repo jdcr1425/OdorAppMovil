e5daa1e61770443d21b00feca6979d79cd6ef520
jdcr1425 <juandavidcr7@gmail.com>
2019-06-13 00:04:16 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View,Image,StatusBar, Button  } from 'react-native';
+import { StyleSheet, Text, View,Image,StatusBar, Button,TouchableOpacity   } from 'react-native';
 import Header from './src/components/Header.js';
 import Constants from 'expo-constants'
 export default class App extends React.Component{
@@ -14,15 +14,21 @@ export default class App extends React.Component{
     <View style={{flex:1}}>
       <View style={styles.statusBar} />
       <Header/>
-      <View style={styles.container}>
+        <View style={{flex:2, justifyContent:'center', backgroundColor:'#ecfffd'}}>
         <Text style={styles.textWithShadow}>¿Percibe algún olor?</Text>
+
         <View style={styles.containerButton}>
-          <Button
-            title="Sí"/>
-          <Button
-            title="No"/>
+
+          <TouchableOpacity style={styles.button}>
+           <Text style={styles.textWithShadow}>SÍ</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[styles.button, {backgroundColor:'#304ffe'}]}>
+           <Text style={styles.textWithShadow}>NO</Text>
+          </TouchableOpacity>
         </View>
-      </View>
+        </View>
+
     </View>
      
     
@@ -40,19 +46,27 @@ const styles = StyleSheet.create({
       statusBar: {
         backgroundColor: "#0026ca",
         height: Constants.statusBarHeight,
+        
       },
       textWithShadow:{
         color:'white',
         fontSize: 40, 
         fontWeight: 'bold',
-        textShadowColor: 'rgba(0, 0, 0, 0.75)',
+        textShadowColor: 'rgba(0, 0, 0, 0.85)',
         textShadowOffset: {width: -1, height: 1},
-        textShadowRadius: 10
+        textShadowRadius: 10,
+        alignSelf:'center'
     },
     containerButton:{
-
+      marginTop:10,
       flexDirection: 'row',
-      justifyContent: 'space-between',
-      backgroundColor:'red'
+      justifyContent: 'space-around',
+    },
+    button:{
+      padding:30,
+      backgroundColor:'#65ba69',
+      borderRadius:70,
+      borderWidth: 1,
+      borderColor: '#fff'
     }
 });
